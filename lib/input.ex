fc394c0aae809cdf29ece91545368b3003802922
BarneyLoosemore <barney.loosemore@pollen.co>
2023-12-05 17:14:44 +0000

    Initialise repo

---
+++ lib/input.ex
@@ -0,0 +1,12 @@
+defmodule Input do
+  defp read_file!(file_name, :sample), do: File.read!("inputs/sample/#{file_name}")
+  defp read_file!(file_name, :sample_pt2), do: File.read!("inputs/sample_pt2/#{file_name}")
+  defp read_file!(file_name, :solution), do: File.read!("inputs/solution/#{file_name}")
+
+  def read!(day, type \\ :solution)
+
+  def read!(day, type) do
+    day = day |> String.pad_leading(2, "0")
+    read_file!("day#{day}.txt", type)
+  end
+end
