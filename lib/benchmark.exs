21ec109ef48987c7e2b941ae8e6e1f643701ffd2
BarneyLoosemore <barney.loosemore@pollen.co>
2023-12-08 17:01:14 +0000

    Add benchmarking lib

---
+++ lib/benchmark.exs
@@ -0,0 +1,9 @@
+Benchee.run(%{
+  "Day 1" => fn -> Day01.pt1(Input.read!(1)) end,
+  "Day 2" => fn -> Day02.pt1(Input.read!(2)) end,
+  "Day 3" => fn -> Day03.pt1(Input.read!(3)) end,
+  "Day 4" => fn -> Day04.pt1(Input.read!(4)) end,
+  "Day 5" => fn -> Day05.pt1(Input.read!(5)) end,
+  "Day 6" => fn -> Day06.pt1(Input.read!(6)) end,
+  "Day 7" => fn -> Day07.pt1(Input.read!(7)) end
+})
--- mix.exs
+++ mix.exs
@@ -21,7 +21,9 @@ defmodule Aoc2023.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false}
+      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:benchee, "~> 1.0", only: :dev},
+      {:math, "~> 0.6.0"}
     ]
   end
 end
